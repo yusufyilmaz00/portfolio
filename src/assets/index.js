a627fa94b396909731fdce53923ae4c12c3461cf
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-04-08 07:25:39 +0300

    update: gerekli icon dosyaları eklendi

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,51 @@
 import logo from './logo.svg';
 import menu from './menu.svg';
 import close from './close.svg';
+import web from './web.svg';
+import mobile from './mobile.svg';
+import backend from './backend.svg';
+import creator from './creator.svg';
+import meta from './company/meta.png';
+import shopify from './company/shopify.png';
+import starbucks from './company/starbucks.png';
+import tesla from './company/tesla.png';
+import javascript from './tech/javascript.png';
+import typescript from './tech/typescript.png';
+import html from './tech/html.png';
+import css from './tech/css.png';
+import reactjs from './tech/reactjs.png';
+import redux from './tech/redux.png';
+import tailwind from './tech/tailwind.png';
+import nodejs from './tech/nodejs.png';
+import mongodb from './tech/mongodb.png';
+import git from './tech/git.png';
+import carrent from "./projects/carrent.png";
+import jobit from "./projects/jobit.png";
+import tripguide from "./projects/tripguide.png";
 
 export {
   logo,
   menu,
   close,
+  web,
+  mobile,
+  backend,
+  creator,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  carrent,
+  jobit,
+  tripguide,
 }; 

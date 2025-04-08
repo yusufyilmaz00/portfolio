684c53b59a98eae2caab9e50653e2ad1b7d2bb1d
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-04-08 07:07:23 +0300

    app.jsx'te import edilen componentler oluşturuldu

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Navbar from './Navbar';
+import Hero from './Hero';
+import About from './About';
+import Experience from './Experience';
+import Projects from './Projects';
+import Contact from './Contact';
+
+export {
+  Navbar,
+  Hero,
+  About,
+  Experience,
+  Projects,
+  Contact,
+}; 

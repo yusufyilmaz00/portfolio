2652c5ef4059faeb0d5660f1db8032a164a2774c
yusufyilmaz00 <yusufyyilmaz0@gmail.com>
2025-04-09 11:47:42 +0300

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,7 @@ import web from './web.svg';
 import mobile from './mobile.svg';
 import backend from './backend.svg';
 import creator from './creator.svg';
+import github from './tech/github.png';
 import meta from './company/meta.svg';
 import shopify from './company/shopify.png';
 import starbucks from './company/starbucks.png';
@@ -31,6 +32,7 @@ export {
   mobile,
   backend,
   creator,
+  github,
   meta,
   shopify,
   starbucks,
